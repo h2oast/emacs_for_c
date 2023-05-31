dbbcc7bfb8f896fcc03f189df2713023d6e28de9
User Vagrant <vagrant@freebsd12.localdomain>
2023-05-31 05:06:15 +0000

    with elgot properly ready

--- init.el
+++ init.el
@@ -1,44 +1,28 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;;
-;;                                        emacs settings for a c/c++ developer
-;;
-;;
-;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+(require 'package)
 (package-initialize)
 
-(require 'package)
+;; Make sure use-package has been installed properly
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; check for emacs version requirement
-(let ((minver "26.3"))
-  (when (version< emacs-version minver)
-    (error "Your Emacs is too old -- this configuration requires VERSION %s or higher" minver)))
+(require 'use-package)
 
-;; add extension load path 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+;; eglot - a lsp client for emacs
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs '((c-mode c++-mode) "clangd"))
+  (add-hook    'c-mode-hook           'eglot-ensure)
+  (add-hook    'c++-mode-hook         'eglot-ensure))
 
-;; packages need to be installed manually first after you installed emacs
-;; setup package except elpa, since others depend on elpa
-(defun develop_initialize()
-    "develop environment initialization for c/c++"
-    (require 'init_zenburn-theme)
-    (require 'init_ecb)
-    (require 'init_cscope)
-    (require 'init_company))
+;; company
+(use-package company
+  :ensure t)
+(use-package yasnippet
+  :ensure t)
 
-;; emacs builtin settings
-(require 'init_emacs)
-(require 'init_elpa)
-(require 'init_site-lisp)
+(use-package magit
+  :ensure t)
 
-;; after you install all the packages need, then can this initialize can act as expected
-(develop_initialize)
-
-
+(setq custom-file ".custom.el")
